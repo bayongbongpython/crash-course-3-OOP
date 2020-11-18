2d9111b7d2336b56c198230a6faf0c73f9bc77dd
WORK <andi.hidayat.mail@gmail.com>
2020-11-18 23:56:15 +0700

    oop

---
+++ geometry/persegipanjang.py
@@ -0,0 +1,11 @@
+class PersegiPanjang():
+    def__init__(self, p, l)
+    #fungsi yang dipanggil per tama kali saat object diciptakan
+        self.p = p
+        self.l = l
+
+    def info(self):
+         return f'Modul menghitung rumus-rumus tentang persegipanjang = {self.p} dan lebar = {self.l}'
+
+    def hitung_luas(self):
+        return self.p * self.l
