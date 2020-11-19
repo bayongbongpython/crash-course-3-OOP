adb1fc3a4bb1716cbf3cdabb669980d71591ab12
WORK <andi.hidayat.mail@gmail.com>
2020-11-19 10:13:07 +0700

    oop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from geometry.persegipanjang import PersegiPanjang
+from geometry.segitiga import Segitiga
 
 print('Mengguna kan OOP')
 p1 = PersegiPanjang(10, 3)
 print(p1.info())
 print(p1.hitung_luas())
+
+s1 = Segitiga(4, 2)
+print(s1.info())
+print(s1.hitung_luas())
